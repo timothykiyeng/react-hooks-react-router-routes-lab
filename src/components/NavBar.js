319ac735b7cff85b08a562e8a0852fc05bfcf4a8
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-29 14:31:17 +0300

    Styled NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,59 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "black",
+};
+
 function NavBar() {
   return (
     <div className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/actors">Actors</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "tomato",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "tomato",
+        }}
+      >
+        Actors
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "tomato",
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "tomato",
+        }}
+      >
+        Movies
+      </NavLink>
     </div>
   );
 }
